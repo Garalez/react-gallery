92835e1f71b708b776bc44606cd49ed204566d05
Vlad Galcev <Vladgalcev03@gmail.com>
2022-11-04 20:36:00 +0200

    hotfix update REDIRECT_URI  from vercel

--- src/api/const.js
+++ src/api/const.js
@@ -5,5 +5,5 @@ export const ACCESS_KEY = 'mzUpcXjeMJFWQJJIcxz98TsB5FDj1W1EdhiTYJRJLG0';
 export const SECRET_KEY = 'FucXnUSi4Gnea3cSjRfxVgzzKPnbaTXwDsfANS4AEes';
 export const RESPONSE_TYPE = 'code';
 export const GRANT_TYPE = 'authorization_code';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'http://react-gallery-nu.vercel.app/auth';
 export const SCOPE = 'public read_user read_photos write_likes';
